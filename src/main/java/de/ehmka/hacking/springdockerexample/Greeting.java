2d0294c6a63e4fa757a75f2c11de2949620773ed
Michael Krausse <michael.krausse@m-krausse.de>
2014-11-11 22:01:12 +0100

    some dummy data classes

---
+++ src/main/java/de/ehmka/hacking/springdockerexample/Greeting.java
@@ -0,0 +1,24 @@
+package de.ehmka.hacking.springdockerexample;
+
+/**
+ * Created by michi on 11/11/14.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
--- src/main/java/de/ehmka/hacking/springdockerexample/HelloController.java
+++ src/main/java/de/ehmka/hacking/springdockerexample/HelloController.java
@@ -1,7 +1,8 @@
 package de.ehmka.hacking.springdockerexample;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by michi on 11/11/14.
@@ -10,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private AtomicLong atomicLong = new AtomicLong();
+
     @RequestMapping("/")
     public String index() {
         return "Greetings from spring boot";
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public  @ResponseBody Greeting sayHello(@RequestParam(value = "name", required = false, defaultValue = "Stranger") String name) {
+        return new Greeting(atomicLong.incrementAndGet(), name);
     }
 }
